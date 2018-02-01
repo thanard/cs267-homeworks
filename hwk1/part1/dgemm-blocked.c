2442b0f35fa091d8ea0e77a8da0b331018e09159
Vipul Gupta <vipul_gupta@berkeley.edu>
2018-01-31 19:09:30 -0800

    transpose and jik

--- hwk1/part1/dgemm-blocked.c
+++ hwk1/part1/dgemm-blocked.c
@@ -9,32 +9,31 @@ CC = cc
 OPT = -O3
 CFLAGS = -Wall -std=gnu99 $(OPT)
 MKLROOT = /opt/intel/composer_xe_2013.1.117/mkl
-LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
+LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKLROOT)
+/lib/intel64/libmkl_sequential.a $(MKLROOT)/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm
 
 */
 #include <stdio.h>
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
-#if !defined(BLOCK_SIZE)
 #define BLOCK_SIZE_2 30
 #define BLOCK_SIZE 100
-#endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
 static void do_block_2 (int lda, int M, int N, int K, double* A, double* B, double* C)
 {  
   /* For each row i of A */
-  for (int i = 0; i < M; ++i)
+  for (int j = 0; j < N; ++j)
     /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
+    for (int i = 0; i < M; ++i) 
     {
       /* Compute C(i,j) */
       double cij = C[i+j*lda];
       for (int k = 0; k < K; ++k)
-      /* Compute C(i,j) */
-      cij += A[i+k*lda] * B[k+j*lda];
+        /* Compute C(i,j) */
+        cij += A[k+i*lda] * B[k+j*lda];
       C[i+j*lda] = cij; 
     }
 
@@ -46,39 +45,54 @@ static void do_block_2 (int lda, int M, int N, int K, double* A, double* B, doub
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; i += BLOCK_SIZE_2)
+  for (int j = 0; i < M; j += BLOCK_SIZE_2)
     /* For each column j of B */ 
-    for (int j = 0; j < N; j += BLOCK_SIZE_2) 
+    for (int i = 0; j < N; i += BLOCK_SIZE_2) 
     {
       for (int k = 0; k < K; k += BLOCK_SIZE_2)
       {
         int M_2 = min (BLOCK_SIZE_2, M-i);
         int N_2 = min (BLOCK_SIZE_2, N-j);
         int K_2 = min (BLOCK_SIZE_2, K-k);
-        do_block_2(lda, M_2, N_2, K_2, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+        do_block_2(lda, M_2, N_2, K_2, A + k + i*lda, B + k + j*lda, C + i + j*lda);
       }
     }
+}
+
+double* transpose (double* A, int lda){
+    int i ,j;
+    double tmp;
+
+    for (i = 0; i < lda; i++) {
+        for (j = 0 ; j < lda; j++) {
+            tmp = A[i+j*lda];
+            A[i+j*lda] = A[j+i*lda];
+            A[j+i*lda] = tmp;
+        }
+    }  
+    return A;
 }
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */  
-void square_dgemm (int lda, double* A, double* B, double* C)
+void square_dgemm (int lda, double* A2, double* B, double* C)
 {
   /* For each block-row of A */ 
-  for (int i = 0; i < lda; i += BLOCK_SIZE)
+  A = transpose(A2, lda)
+  for (int j = 0; i < lda; j += BLOCK_SIZE)
     /* For each block-column of B */
-    for (int j = 0; j < lda; j += BLOCK_SIZE)
+    for (int i = 0; j < lda; i += BLOCK_SIZE)
       /* Accumulate block dgemms into block of C */
       for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
-	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
+      	/* Correct block dimensions if block "goes off edge of" the matrix */
+      	int M = min (BLOCK_SIZE, lda-i);
+      	int N = min (BLOCK_SIZE, lda-j);
+      	int K = min (BLOCK_SIZE, lda-k);
 
-	/* Perform individual block dgemm */
-	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+      	/* Perform individual block dgemm */
+      	do_block(lda, M, N, K, A + k + i*lda, B + k + j*lda, C + i + j*lda);
       }
 }
