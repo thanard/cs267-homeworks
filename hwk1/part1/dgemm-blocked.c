eb90123a12c22b5b91458fad72a6d81ccc303729
thanard <thanard.kurutach@berkeley.edu>
2018-02-03 17:10:58 -0800

    Got speed up by not copying A or B. Check if we need 2 layers of do block or not.

--- hwk1/part1/dgemm-blocked.c
+++ hwk1/part1/dgemm-blocked.c
@@ -36,11 +36,11 @@ static void print_matrix(double* A, int M, int N, int lda){
   printf("\n");
 }
 
-static void avx_mult(double* A, double* B, double* C){
+static void avx_mult(double* A, double* B, double* C, int lda){
   __m256d a1 = _mm256_loadu_pd(A);
-  __m256d a2 = _mm256_loadu_pd(A+4);
-  __m256d a3 = _mm256_loadu_pd(A+8);
-  __m256d a4 = _mm256_loadu_pd(A+12);
+  __m256d a2 = _mm256_loadu_pd(A+lda);
+  __m256d a3 = _mm256_loadu_pd(A+2*lda);
+  __m256d a4 = _mm256_loadu_pd(A+3*lda);
 
   __m256d tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B));
   tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+1), tmp);
@@ -49,49 +49,27 @@ static void avx_mult(double* A, double* B, double* C){
   _mm256_store_pd(C, _mm256_add_pd(_mm256_loadu_pd(C), tmp));
 
   // C+4
-  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+4));
-  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+5), tmp);
-  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+6), tmp);
-  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+7), tmp);
+  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+lda));
+  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+lda+1), tmp);
+  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+lda+2), tmp);
+  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+lda+3), tmp);
   _mm256_store_pd(C+4, _mm256_add_pd(_mm256_loadu_pd(C+4), tmp));    
 
   // C+8
-  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+8));
-  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+9), tmp);
-  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+10), tmp);
-  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+11), tmp);
+  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+2*lda));
+  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+2*lda+1), tmp);
+  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+2*lda+2), tmp);
+  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+2*lda+3), tmp);
   _mm256_store_pd(C+8, _mm256_add_pd(_mm256_loadu_pd(C+8), tmp));
 
+
   // C+4
-  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+12));
-  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+13), tmp);
-  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+14), tmp);
-  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+15), tmp);
+  tmp = _mm256_mul_pd(a1, _mm256_broadcast_sd(B+3*lda));
+  tmp = _mm256_fmadd_pd(a2, _mm256_broadcast_sd(B+3*lda+1), tmp);
+  tmp = _mm256_fmadd_pd(a3, _mm256_broadcast_sd(B+3*lda+2), tmp);
+  tmp = _mm256_fmadd_pd(a4, _mm256_broadcast_sd(B+3*lda+3), tmp);
   _mm256_store_pd(C+12, _mm256_add_pd(_mm256_loadu_pd(C+12), tmp));
 
-}
-
-static void writeto4by4(double* small, double* big, int i, int j, int lda){
-  // Write to small from big.
-  *(small) = *(big+i+j*lda);
-  *(small+1) = *(big+i+1+j*lda);
-  *(small+2) = *(big+i+2+j*lda);
-  *(small+3) = *(big+i+3+j*lda);
-
-  *(small+4) = *(big+i+j*lda+lda);
-  *(small+5) = *(big+i+1+j*lda+lda);
-  *(small+6) = *(big+i+2+j*lda+lda);
-  *(small+7) = *(big+i+3+j*lda+lda);
-  
-  *(small+8) = *(big+i+j*lda + 2*lda);
-  *(small+9) = *(big+i+1+j*lda + 2*lda);
-  *(small+10) = *(big+i+2+j*lda + 2*lda);
-  *(small+11) = *(big+i+3+j*lda + 2*lda);
-
-  *(small+12) = *(big+i+j*lda + 3*lda);
-  *(small+13) = *(big+i+1+j*lda + 3*lda);
-  *(small+14) = *(big+i+2+j*lda + 3*lda);
-  *(small+15) = *(big+i+3+j*lda + 3*lda);
 }
 
 static void addfrom4by4(double* small, double* big, int i, int j, int lda){
@@ -131,10 +109,11 @@ static void do_block_2 (int lda, int M, int N, int K, double* A, double* B, doub
       /* Compute C(i,j) */
       for (int k = 0; k < K; k += 4)
       {
-        writeto4by4(tempA, A, i, k, lda);
-        writeto4by4(tempB, B, k, j, lda);
-        // printf("i = %d, j = %d, k = %d, A = %.3lf, B = %.3lf, C = %.3lf \n", i, j, k, *(tempA+i+k*lda), *(tempB+k+j*lda), *(tempC+i+j*lda));
-        avx_mult(tempA, tempB, tempC);
+        // writeto4by4(tempA, A, i, k, lda);
+        // writeto4by4(tempB, B, k, j, lda);
+        // // printf("i = %d, j = %d, k = %d, A = %.3lf, B = %.3lf, C = %.3lf \n", i, j, k, *(tempA+i+k*lda), *(tempB+k+j*lda), *(tempC+i+j*lda));
+        // avx_mult(tempA, tempB, tempC);
+        avx_mult(A+i+k*lda, B+k+j*lda, tempC, lda);
       }
       // Add back to C matrix
       addfrom4by4(tempC, C, i, j, lda);
@@ -170,7 +149,7 @@ void square_dgemm (int lda, double* A, double* B, double* C)
 {
   // print_matrix(A, lda, lda, lda);
   // print_matrix(B, lda, lda, lda);
-    // avx_mult(A, B, C);
+    // avx_mult(A, B, C, lda);
   /* For each block-row of A */ 
   for (int j = 0; j < lda; j += BLOCK_SIZE)
     /* For each block-column of B */
